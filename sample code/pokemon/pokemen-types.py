2c1e4b7961b03f3f8a814b07e7e0bdf54ca7c130
Yvonne <yvonne.yangben@gmail.com>
2023-05-09 22:18:27 +0800

    Added the pokemon-types file

---
+++ sample code/pokemon/pokemen-types.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+
+# fetch the api data and convert to dictionary:
+api_url = "https://pokeapi.co/api/v2/type/ghost"
+ghost_data = requests.get(api_url).json()
+
+"""# display all key-value pairs from the JSON data:
+for key in ghost_data:
+    print("key: ", key, "\n", "value:", ghost_data[key], "\n")
+
+# display damage relations for this type:
+print(ghost_data["damage_relations"])"""
+
+# display 'double_damage_from' in damage relations:
+for double_damage_from in ghost_data["damage_relations"]["double_damage_from"]:
+    print(double_damage_from["name"])
+
+# display 'double_damage_to' in damage relations:
+for double_damage_to in ghost_data["damage_relations"]["double_damage_to"]:
+    print(double_damage_to["name"])
