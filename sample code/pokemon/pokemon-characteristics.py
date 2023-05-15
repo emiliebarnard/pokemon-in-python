7c3f6e012121c7a7e912ed592fc7d9ff0cbbc679
Emilie Menard Barnard <emiliebarnard@gmail.com>
2023-05-15 00:13:45 -0700

    grabbing keys

--- sample code/pokemon/pokemon-characteristics.py
+++ sample code/pokemon/pokemon-characteristics.py
@@ -4,6 +4,9 @@ import requests, json
 api_url = "https://pokeapi.co/api/v2/characteristic/4"
 highly_curious_data = requests.get(api_url).json()
 
-# display all key-value pairs from the JSON data:
+# # display all key-value pairs from the JSON data:
+# for key in highly_curious_data:
+#     print("key: ", key, "\n", "value:", highly_curious_data[key], "\n")
+
 for key in highly_curious_data:
-    print("key: ", key, "\n", "value:", highly_curious_data[key], "\n")
+     print(key)
