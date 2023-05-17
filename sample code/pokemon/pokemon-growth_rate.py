69e88a5660372dcf7e9aa494df7fe67a6e107d55
Emilie Menard Barnard <emiliebarnard@gmail.com>
2023-05-16 22:10:08 -0700

    base growth rate code

---
+++ sample code/pokemon/pokemon-growth_rate.py
@@ -0,0 +1,9 @@
+import requests, json
+
+# fetch the api data and convert to dictionary:
+api_url = "https://pokeapi.co/api/v2/growth-rate/5"
+erratic_data = requests.get(api_url).json()
+
+# display all key-value pairs from the JSON data:
+for key in erratic_data:
+    print("key: ", key, "\n", "value:", erratic_data[key], "\n")
