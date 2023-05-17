5f2813ecfacec375bc260598f962ef800be81f5f
Emilie Menard Barnard <emiliebarnard@gmail.com>
2023-05-16 21:47:08 -0700

    sample gender code added

--- sample code/pokemon/pokemon-genders.py
+++ sample code/pokemon/pokemon-genders.py
@@ -5,5 +5,14 @@ api_url = "https://pokeapi.co/api/v2/gender/female/"
 female_data = requests.get(api_url).json()
 
 # display all key-value pairs from the JSON data:
-for key in female_data:
-    print("key: ", key, "\n", "value:", female_data[key], "\n")
+# for key in female_data:
+#     print("key: ", key, "\n", "value:", female_data[key], "\n")
+
+def gendered_evolution(pokemon):
+    for poke in female_data["required_for_evolution"]:
+        if poke["name"] == pokemon:
+            return True
+    return False
+
+print(gendered_evolution("wormadam"))
+print(gendered_evolution("skitty"))
