4ba3ea94cb22e0ba51a26395f6b66ecb4f2c0029
Emilie Menard Barnard <emiliebarnard@gmail.com>
2023-05-16 22:14:17 -0700

    added sample code for growth rate

--- sample code/pokemon/pokemon-growth_rate.py
+++ sample code/pokemon/pokemon-growth_rate.py
@@ -4,6 +4,9 @@ import requests, json
 api_url = "https://pokeapi.co/api/v2/growth-rate/5"
 erratic_data = requests.get(api_url).json()
 
-# display all key-value pairs from the JSON data:
-for key in erratic_data:
-    print("key: ", key, "\n", "value:", erratic_data[key], "\n")
+# # display all key-value pairs from the JSON data:
+# for key in erratic_data:
+#     print("key: ", key, "\n", "value:", erratic_data[key], "\n")
+
+for pokemon in erratic_data["pokemon_species"]:
+    print(pokemon["name"])
